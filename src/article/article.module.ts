07e8041f4adea9bf563b7f071ad967a4e382c28b
MaxRyazan <no4ta87@mail.ru>
2024-02-22 23:05:58 +0300

    1.1.1 drop script refactoring

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Publisher } from '@entities/publisher.entity';
+import { Article } from '@entities/article.entity';
 
 @Module({
   controllers: [ArticleController],
   providers: [ArticleService],
+  imports: [TypeOrmModule.forFeature([Article])]
 })
 export class ArticleModule {}
